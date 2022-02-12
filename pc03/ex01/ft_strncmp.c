b11be2a1c497c4ed26d5a9421c9069547ea15fe8
Osongmin <autumninmoon@gmail.com>
2022-02-13 00:19:08 +0900

    2/13

--- pc03/ex01/ft_strncmp.c
+++ pc03/ex01/ft_strncmp.c
@@ -6,44 +6,18 @@
 /*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 20:30:41 by soo               #+#    #+#             */
-/*   Updated: 2022/02/10 21:19:31 by soo              ###   ########.fr       */
+/*   Updated: 2022/02/13 00:03:31 by songmin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	is_check(char *s1, char *s2, unsigned int n)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (s1[i] && s2[i])
-		++i;
-	if (i > n)
-		return (1);
-	return (0);
-}
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	if (is_check(s1, s2, n))
-	{
-		while (i < n)
-		{
-			if (! (s1[i] == s2[i] && s1[i] && s2[i]))
-				break ;
-			++i;
-		}
-	}
-	else
-	{
-		while (s1[i] && s2[i])
-		{
-			if (s1[i] != s2[i])
-				break ;
-			++i;
-		}
-	}
+	if (n <= 0)
+		return (0);
+	while (i + 1 < n && s1[i] && s2[i] && s1[i] == s2[i])
+		++i;	
 	return (s1[i] - s2[i]);
 }
--- pc03/ex04/ft_strstr.c
+++ pc03/ex04/ft_strstr.c
@@ -1,34 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 15:55:07 by soo               #+#    #+#             */
+/*   Updated: 2022/02/13 00:18:38 by songmin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	get_len(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		++len;
+	return (len);
+}
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	char	*sp;
-	char	*fp;
-	int	flag;
+	char *sp;
+	char *fp;
 
-	flag = 0;
 	if (*to_find == 0)
 		return (str);
+	if (get_len(str) < get_len(to_find))
+		return (0);
 	while (*str)
 	{
 		while (*str != *to_find)
 			++str;
 		sp = str;
 		fp = to_find;
-		while (*fp)
+		while (*fp && (*sp == *fp))
 		{
-			if (*fp == *sp)
-				flag = 1;
-			else
-			{
-				flag = 0;
-				break ;
-			}
-			++fp;
 			++sp;
+			++fp;
 		}
-		if (flag)
+		if (*fp == 0)
 			return (str);
 		++str;
 	}
 	return (0);
 }
+
